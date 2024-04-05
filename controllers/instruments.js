1f6ee1df83da110a240b03f44f626d98989ec7d3
DineshMargana <dineshmargana@gmail.com>
2024-04-05 17:48:10 -0500

    commit

--- controllers/instruments.js
+++ controllers/instruments.js
@@ -1,26 +1,51 @@
-var instruments= require('../models/instruments');
-// List of all food
+
+var instruments = require('../models/instruments');
+// List of all instruments
 exports.instruments_list = function(req, res) {
-res.send('NOT IMPLEMENTED: instruments list');
+ res.send('NOT IMPLEMENTED: instruments list');
 };
-// for a specific Dog.
-exports.instruments_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: instruments detail: ' + req.params.id);
+// for a specific instruments.
+exports.instruments_detail = async function(req, res) {
+console.log("detail" + req.params.id)
+try {
+result = await instruments.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
 };
-// Handle Dog create on POST.
+
+// Handle instruments create on POST.
 exports.instruments_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: instruments create POST');
+ res.send('NOT IMPLEMENTED: instruments create POST');
 };
-// Handle Dog delete from on DELETE.
+// Handle instruments delete from on DELETE.
 exports.instruments_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: instruments delete DELETE ' + req.params.id);
+ res.send('NOT IMPLEMENTED: instruments delete DELETE ' + req.params.id);
 };
-// Handle Dog update form on PUT.
-exports.instruments_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: instruments update PUT' + req.params.id);
+// Handle instruments update form on PUT.
+exports.instruments_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Costume.findById( req.params.id)
+// Do updates of properties
+if(req.body.instruments_type)
+toUpdate.instruments_type = req.body.costume_type;
+if(req.body.instruments_size) toUpdate.cost = req.body.instruments_size;
+if(req.body.instruments_price) toUpdate.size = req.body.instruments_price;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
- 
- 
+
+
 exports.instruments_list = async function(req, res) {
     try{
     theinstruments = await instruments.find();
@@ -32,6 +57,7 @@ exports.instruments_list = async function(req, res) {
     }
    };
  
+ 
 exports.instruments_view_all_Page = async function(req, res) {
     try{
     theinstruments = await instruments.find();
@@ -42,9 +68,9 @@ exports.instruments_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
- 
- 
-    // Handle food create on POST.
+
+
+    // Handle instruments create on POST.
 exports.instruments_create_post = async function(req, res) {
     console.log(req.body)
     let document = new instruments();
@@ -54,8 +80,6 @@ exports.instruments_create_post = async function(req, res) {
     document.instrument_type = req.body.instrument_type;
     document.brand = req.body.brand;
     document.price = req.body.price;
-    
- 
     try{
     let result = await document.save();
     res.send(result);
@@ -65,3 +89,4 @@ exports.instruments_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+    
