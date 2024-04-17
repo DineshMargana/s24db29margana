31bfeef8c0a6cac1e1b3235334986a67a840ffe1
DineshMargana <dineshmargana@gmail.com>
2024-04-17 17:15:24 -0500

    commit

--- models/instruments.js
+++ models/instruments.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose")
 const instrumentsSchema = mongoose.Schema({
-instrument_type: String,
-brand: String,
-price: Number
+    instrument_type: {
+        type: String,
+        minlength: 2,
+        maxlength: 14,
+
+    },
+    brand:{
+        type: String,
+        minlength: 2,
+        maxlength: 14,
+
+    },
+    price: Number
 })
 module.exports = mongoose.model("instruments",
-instrumentsSchema)
+    instrumentsSchema)
--- routes/instruments.js
+++ routes/instruments.js
@@ -1,22 +1,36 @@
 var express = require('express');
-const instruments_controlers= require('../controllers/instruments');
+var passport = require('passport');
+const instruments_controlers = require('../controllers/instruments');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
 /* GET instruments */
- 
+
 router.get('/', instruments_controlers.instruments_view_all_Page);
- 
-router.put('/', function(req, res) {
-    if(req.body.checkboxsale)toUpdate.sale = true;
+
+router.put('/', function (req, res) {
+    if (req.body.checkboxsale) toUpdate.sale = true;
     else toUpdate.same = false;
-    })
-router.get('/', instruments_controlers.instruments_delete );
+})
+router.get('/', instruments_controlers.instruments_delete);
 /* GET detail instruments page */
 router.get('/detail', instruments_controlers.instruments_view_one_Page);
 
 /* GET create costume page */
 router.get('/create', instruments_controlers.instruments_create_Page);
 
-router.get('/update', instruments_controlers.instruments_update_Page);
+router.get('/update', secured, instruments_controlers.instruments_update_Page);
+router.post('/login', passport.authenticate('local'), function (req, res) {
+    res.redirect('/');
+});
+
+
 
 router.get('/delete', instruments_controlers.instruments_delete_Page);
 module.exports = router;
